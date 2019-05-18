5815af3324c86fdaaff0f1ee3f2a2ebf12eeb6fd
Brandon Botezatu <decadeofdefiance@gmail.com>
2019-05-17 23:57:21 -0500

    added slider

--- colortemp.py
+++ colortemp.py
@@ -16,20 +16,22 @@ selectedTemp = ""
 def emptyTextFieldCallBack():
         messagebox.showinfo("Warning", "Missing a value between 5000 and 25000")
 
-def youKickAssCallBack():
-        messagebox.showinfo("", "You're awesome!")
-
 def changeScreenColorTemp():
         if entry.get() == "":
                 messagebox.showinfo("Warning", "Field cannot be empty")
         elif not entry.get().isdigit():
                 messagebox.showinfo("Warning", "Can only contain numerical values")
         elif int(entry.get()) > 20000 or int(entry.get()) < 1000:
-                messagebox.showinfo("Warning", "Can only contain values between 1000 and 25000.")
+                messagebox.showinfo("Warning", "Can only contain values between 1000 and 20000.")
         else:
                 selectedTemp = entry.get()
                 sub.call('redshift -O %s' % selectedTemp ,shell=True)
 
+def safeScreenChange(safeTemp):
+	sub.call('redshift -O %s' % safeTemp ,shell=True)
+
+
+	
 close_button = tkinter.Button(root, text = "Quit", command=quit)
 
 entry = tkinter.Entry(root)
@@ -39,10 +41,19 @@ entry.pack()
 
 change_screen_button = tkinter.Button(root, text = "Change Screen Temp", command=changeScreenColorTemp)
 change_screen_button.pack()
-change_screen_button.config(height = 10, width = 25)
-close_button.pack()
-close_button.config(height=2, width=6)
+change_screen_button.config(height = 3, width = 25)
 
+"""
+min_button  = tkinter.Button(root, text="1000(minimum)", command= lambda: safeScreenChange(1000))
+min_button.pack()
+min_button.config(height=2, width=10)
+"""
+
+scale = tkinter.Scale(orient='horizontal', from_=1000, to=20000, length=400, width=50, command=safeScreenChange)
+scale.pack()
+
+close_button.pack()
+close_button.config(height=4, width=10)
 
 root.mainloop()
 
