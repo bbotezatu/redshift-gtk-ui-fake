f9ddbb318c2abcae264569e3d4ab62bbb6dd3bdb
Brandon Botezatu <bbotezatu@protonmail.com>
2020-03-06 16:11:13 -0600

    fixed default value for slider

--- colortemp.py
+++ colortemp.py
@@ -12,25 +12,31 @@ root.geometry('300x300')
 
 
 root.title("Brandon's Redshift UI")
+
+
 selectedTemp = ""
 def emptyTextFieldCallBack():
         messagebox.showinfo("Warning", "Missing a value between 5000 and 25000")
 
 def changeScreenColorTemp():
         if entry.get() == "":
-                messagebox.showinfo("Warning", "Field cannot be empty")
+                messagebox.showinfo("Error", "Field cannot be empty")
         elif not entry.get().isdigit():
-                messagebox.showinfo("Warning", "Can only contain numerical values")
+                messagebox.showinfo("Error", "Can only contain numerical values")
         elif int(entry.get()) > 20000 or int(entry.get()) < 1000:
-                messagebox.showinfo("Warning", "Can only contain values between 1000 and 20000.")
+                messagebox.showinfo("Error", "Can only contain values between 1000 and 20000.")
         else:
                 selectedTemp = entry.get()
-                sub.call('redshift -O %s' % selectedTemp ,shell=True)
+                #sub.call('redshift -O %s' % selectedTemp ,shell=True)
+                safeScreenChange(selectedTemp)
 
 def safeScreenChange(safeTemp):
 	sub.call('redshift -O %s' % safeTemp ,shell=True)
+	scale.pack()
+	tmp = safeTemp
+	scale.set(tmp)
 
-
+scale = tkinter.Scale(orient='horizontal', from_=1000, to=20000, length=400, width=50, command=safeScreenChange)
 	
 close_button = tkinter.Button(root, text = "Quit", command=quit)
 
@@ -49,8 +55,9 @@ min_button.pack()
 min_button.config(height=2, width=10)
 """
 
-scale = tkinter.Scale(orient='horizontal', from_=1000, to=20000, length=400, width=50, command=safeScreenChange)
+
 scale.pack()
+scale.set(5000)
 
 close_button.pack()
 close_button.config(height=4, width=10)
